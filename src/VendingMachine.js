9b1b9dac45175162e6599025c1c25d035652390c
Christian Cruz <christian.cruz92@hotmail.com>
2024-07-04 10:38:07 -0400

    added vending maching component and added some small features and css

---
+++ src/VendingMachine.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VendingMachine = () => {
+    return(
+        <div className="vendMachDiv">
+            <h1>BIG BOY DELUXE VENDING</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default VendingMachine;
